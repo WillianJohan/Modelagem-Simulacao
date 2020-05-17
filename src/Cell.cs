d3e21ffce85c07d0e28874fc8ed9e66acb22e187
willianjohan <johan_willian@hotmail.com>
2020-05-17 14:48:12 -0300

    nova versão adicionada

---
+++ src/Cell.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public enum Estado
+    {
+        adormecida = 0,
+        viva,
+        morta
+    };
+
+    [HideInInspector] public Estado estado = Estado.adormecida;
+    public Sprite[] sprite_De_Estados;
+
+    public void updateStatus(int vizinhosVivos)
+    {
+        if (vizinhosVivos == 1 || vizinhosVivos == 2) nascer();
+        if (vizinhosVivos == 3 || vizinhosVivos == 4) morrer();
+    }
+
+    public void nascer()
+    {
+        if (estado == Estado.viva) return;
+        estado = Estado.viva;
+        updateSprite();
+    }
+
+    public void morrer()
+    {
+        if (estado == Estado.morta) return;
+        estado = Estado.morta;
+        updateSprite();
+    }
+
+    private void updateSprite()
+    {
+        SpriteRenderer myRender = GetComponent<SpriteRenderer>();
+        myRender.sprite = sprite_De_Estados[(int)estado];
+    }
+
+}
---
+++ src/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Grid grid = null;
+    public GameObject cellPrefab;
+
+    public Slider slider_XGridSize;
+    public Text slider_Xtext;
+    public Slider slider_YGridSize;
+    public Text slider_Ytext;
+
+    private void Start()
+    {
+        grid = new Grid();
+    }
+
+    public void newGrid()
+    {
+        grid.destroyCells();
+        grid.generateNewGrid((int)slider_XGridSize.value, (int)slider_YGridSize.value, cellPrefab);
+        grid.initializeRandomPos();
+    }
+
+    public void setText_X()
+    {
+        slider_Xtext.text = slider_XGridSize.value.ToString();
+    }
+    public void setText_Y()
+    {
+        slider_Ytext.text = slider_YGridSize.value.ToString();
+    }
+
+    public void nextGen()
+    {
+        if (!grid) grid.runNextGeneration();
+    }
+
+}
